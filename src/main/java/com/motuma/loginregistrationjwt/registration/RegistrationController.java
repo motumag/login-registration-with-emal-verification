3d331b0e749fb7bb0ee57632737cd1929dff2289
motumag <gishumotuma@gmail.com>
2023-01-22 10:30:58 +0300

    password and websecurity config added

--- src/main/java/com/motuma/loginregistrationjwt/registration/RegistrationController.java
+++ src/main/java/com/motuma/loginregistrationjwt/registration/RegistrationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "registration/v1/")
 @AllArgsConstructor
 public class RegistrationController {
+    private RegistrationService registrationService;
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
--- src/main/java/com/motuma/loginregistrationjwt/registration/RegistrationService.java
+++ src/main/java/com/motuma/loginregistrationjwt/registration/RegistrationService.java
@@ -1,4 +1,7 @@
 package com.motuma.loginregistrationjwt.registration;
 
 public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
 }

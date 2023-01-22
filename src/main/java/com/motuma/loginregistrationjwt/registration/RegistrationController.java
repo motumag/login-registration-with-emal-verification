ae4ec7fabecbde962114de8e088696eab85096da
motumag <gishumotuma@gmail.com>
2023-01-22 10:11:36 +0300

    RegistrationRequest and RegistrationService added

--- src/main/java/com/motuma/loginregistrationjwt/registration/RegistrationController.java
+++ src/main/java/com/motuma/loginregistrationjwt/registration/RegistrationController.java
@@ -1,7 +1,15 @@
 package com.motuma.loginregistrationjwt.registration;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "registration/v1/")
+@AllArgsConstructor
 public class RegistrationController {
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
 }
---
+++ src/main/java/com/motuma/loginregistrationjwt/registration/RegistrationRequest.java
@@ -0,0 +1,4 @@
+package com.motuma.loginregistrationjwt.registration;
+
+public class RegistrationRequest {
+}
